a221c753ff1e97fc89564616c9dc9b5ec6c14f03
JamilaRowlandChandler <jr772@cantab.ac.uk>
2024-07-04 18:49:59 +0100

    update 04/07/2024

--- Ecological-Dynamics/Chemically-mediated models/chemically_mediated_interaction_mechanisms.py
+++ Ecological-Dynamics/Chemically-mediated models/chemically_mediated_interaction_mechanisms.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
+import matplotlib.patheffects as path_effects
 from scipy.integrate import solve_ivp
 import matplotlib as mpl
 
@@ -206,15 +207,15 @@ def cross_feeding():
     yields = simulate_dynamics(two_species_model,
                                fixed_growth,variable_growth,consumption,production,
                                influx,outflux,
-                               30)
+                               40)
 
     species_interaction_df10 = species_interactions(yields, 10)
-    species_interaction_df30 = species_interactions(yields, 30)
+    species_interaction_df40 = species_interactions(yields, 40)
 
     species_interaction_df10['Variable growth rate (2)'] = variable_growth[0,:]
-    species_interaction_df30['Variable growth rate (2)'] = variable_growth[0,:]
+    species_interaction_df40['Variable growth rate (2)'] = variable_growth[0,:]
     
-    return species_interaction_df10, species_interaction_df30
+    return yields, species_interaction_df10, species_interaction_df40
 
 #%%
 
@@ -245,16 +246,16 @@ def cross_feeding_saturation():
     yields = simulate_dynamics(two_species_model_saturating_effects,
                                fixed_growth,variable_growth,consumption,production,
                                influx,outflux,
-                               30,
+                               40,
                                K=1)
 
     species_interaction_df10 = species_interactions(yields, 10)
-    species_interaction_df30 = species_interactions(yields, 30)
+    species_interaction_df40 = species_interactions(yields, 40)
 
     species_interaction_df10['Variable growth rate (2)'] = variable_growth[0,:]
-    species_interaction_df30['Variable growth rate (2)'] = variable_growth[0,:]
+    species_interaction_df40['Variable growth rate (2)'] = variable_growth[0,:]
     
-    return species_interaction_df10, species_interaction_df30
+    return yields, species_interaction_df10, species_interaction_df40
 
 #%%
 
@@ -285,15 +286,15 @@ def cross_feeding_with_inhibition():
     yields = simulate_dynamics(two_species_model,
                                fixed_growth,variable_growth,consumption,production,
                                influx,outflux,
-                               30)
+                               40)
 
     species_interaction_df10 = species_interactions(yields, 10)
-    species_interaction_df30 = species_interactions(yields, 30)
+    species_interaction_df40 = species_interactions(yields, 40)
 
     species_interaction_df10['Variable growth rate (2)'] = variable_growth[0,:]
-    species_interaction_df30['Variable growth rate (2)'] = variable_growth[0,:]
+    species_interaction_df40['Variable growth rate (2)'] = variable_growth[0,:]
     
-    return species_interaction_df10, species_interaction_df30
+    return yields, species_interaction_df10, species_interaction_df40
 
 #%%
 
@@ -324,181 +325,251 @@ def cross_feeding_with_inhibition_saturation():
     yields = simulate_dynamics(two_species_model_saturating_effects,
                                fixed_growth,variable_growth,consumption,production,
                                influx,outflux,
-                               30,K=1)
+                               40,K=1)
 
     species_interaction_df10 = species_interactions(yields, 10)
-    species_interaction_df30 = species_interactions(yields, 30)
+    species_interaction_df40 = species_interactions(yields, 40)
 
     species_interaction_df10['Variable growth rate (2)'] = variable_growth[0,:]
-    species_interaction_df30['Variable growth rate (2)'] = variable_growth[0,:]
+    species_interaction_df40['Variable growth rate (2)'] = variable_growth[0,:]
     
-    return species_interaction_df10, species_interaction_df30
+    return yields, species_interaction_df10, species_interaction_df40
+
+#%%
+    
+yields_cf, crossfeeding10, crossfeeding40 = cross_feeding()
+yields_cf_sat, crossfeeding_saturation10, crossfeeding_saturation40 = \
+    cross_feeding_saturation()
+yields_inhib, inhibition10, inhibition40 = cross_feeding_with_inhibition()
+yields_inhib_sat, inhibition_saturation10,inhibition_saturation40 = \
+    cross_feeding_with_inhibition_saturation() 
+
+# Non-saturating
+
+plot_crossfeeding10 = plot_df(crossfeeding10)
+plot_crossfeeding40 = plot_df(crossfeeding40)
+plot_inhibition10 = plot_df(inhibition10)
+plot_inhibition40 = plot_df(inhibition40)
+
+plot_inhibition10['Species'] = np.repeat([2,1],inhibition10.shape[0])
+plot_inhibition40['Species'] = np.repeat([2,1],inhibition40.shape[0])
+
+# Saturating
+
+plot_crossfeeding_sat10 = plot_df(crossfeeding_saturation10)
+plot_crossfeeding_sat40 = plot_df(crossfeeding_saturation40)
+plot_inhibition_sat10 = plot_df(inhibition_saturation10)
+plot_inhibition_sat40 = plot_df(inhibition_saturation40)
+
+plot_inhibition_sat10['Species'] = np.repeat([2,1],inhibition_saturation10.shape[0])
+plot_inhibition_sat40['Species'] = np.repeat([2,1],inhibition_saturation40.shape[0])
 
 #%%
 
-############# Plotting ##################
+############ Plot  ###############
 
-def main():
-    
-    crossfeeding10, crossfeeding30 = cross_feeding()
-    crossfeeding_saturation10, crossfeeding_saturation30 = cross_feeding_saturation()
-    inhibition10, inhibition30 = cross_feeding_with_inhibition()
-    inhibition_saturation10,inhibition_saturation30 = cross_feeding_with_inhibition_saturation() 
-    
-    # Non-saturating
-    
-    plot_crossfeeding10 = plot_df(crossfeeding10)
-    plot_crossfeeding30 = plot_df(crossfeeding30)
-    plot_inhibition10 = plot_df(inhibition10)
-    plot_inhibition30 = plot_df(inhibition30)
-    
-    plot_inhibition10['Species'] = np.repeat([2,1],inhibition10.shape[0])
-    plot_inhibition30['Species'] = np.repeat([2,1],inhibition30.shape[0])
-    
-    # Saturating
-    
-    plot_crossfeeding_sat10 = plot_df(crossfeeding_saturation10)
-    plot_crossfeeding_sat30 = plot_df(crossfeeding_saturation30)
-    plot_inhibition_sat10 = plot_df(inhibition_saturation10)
-    plot_inhibition_sat30 = plot_df(inhibition_saturation30)
-    
-    plot_inhibition_sat10['Species'] = np.repeat([2,1],inhibition_saturation10.shape[0])
-    plot_inhibition_sat30['Species'] = np.repeat([2,1],inhibition_saturation30.shape[0])
-    
-    ############ Plot  ###############
-    
-    x = 'Variable monoculture yield'
-    y = 'Coculture effect'
-    
-    cpal = sns.color_palette("yb",n_colors=2)
-    
-    # Non-saturating
-    
-    sns.set_style('white')
-    
-    fig = plt.figure(layout='constrained', figsize=(7, 6.25))
-    subfigs = fig.subfigures(2, 1, wspace=0.1)
-    
-    axes_top = subfigs[0].subplots(1, 2, sharex=True, sharey=True)
+x = 'Variable monoculture yield'
+y = 'Coculture effect'
 
-    for df, ax in zip([plot_crossfeeding10, plot_crossfeeding30],
-                      axes_top.flatten()):
-        
-        ax.hlines(0,xmin=df[x].min(),xmax=df[x].max(),
-                  color='gray',linestyle='--')
+cpal = sns.color_palette("yb",n_colors=2)
 
-        sns.lineplot(data = df, x = x, y = y, hue = 'Species',
-                     palette = cpal, marker = 'o', linewidth = 2.5,
-                     markersize = 8, ax = ax)
-        
-        ax.set(xlabel=None)
-        ax.set(ylabel=None)
-        ax.legend_.remove()
-    
-    subfigs[0].suptitle('Non-reciprocal cross-feeding drives commensalist and parasitic \n species interactions.',
-                        fontsize=14, fontweight = 'bold')
-    subfigs[0].supxlabel('Monoculture yield of species 1', fontsize=14)
-    axes_top[0].set_title('time = 10', fontsize=13)
-    axes_top[1].set_title('time = 30', fontsize=13)
-    
-    ####
-    
-    axes_bottom = subfigs[1].subplots(1, 2, sharex=True, sharey=True)
+#%%
 
-    for df, ax in zip([plot_inhibition10, plot_inhibition30],
-                      axes_bottom.flatten()):
-        
-        ax.hlines(0,xmin=df[x].min(),xmax=df[x].max(),
-                  color='gray',linestyle='--')
+# Non-saturating
 
-        sns.lineplot(data = df, x = x, y = y, hue = 'Species',
-                     palette = cpal, marker = 'o', linewidth = 2.5,
-                     markersize = 8, ax = ax)
-        
-        ax.set(xlabel=None)
-        ax.set(ylabel=None)
-        ax.legend_.remove()
-        
-    subfigs[1].suptitle('Cross-feeding on an inhibiting waste product drives \n mutualistic and commensalist species interactions.',
-                        fontsize=14, fontweight='bold')
-    subfigs[1].supxlabel('Monoculture yield of species 2', fontsize=14)
-    axes_bottom[0].set_title('time = 10', fontsize=13)
-    axes_bottom[1].set_title('time = 30', fontsize=13)
-    
-    fig.supylabel(r'$log_2(\frac{\text{coculture yield}}{\text{monoculture yield}})$',multialignment='center',
-                  fontsize=18)
-    
-    sns.despine()
-    
-    plt.savefig("C:/Users/jamil/Documents/PhD/Data files and figures/Ecological-Dynamics-and-Community-Selection/Ecological Dynamics/Figures/speciesinteractions.png",
-                dpi=300,bbox_inches='tight')
-    
-    ####################
-    
-    # Saturating 
-    
-    sns.set_style('white')
-    
-    fig = plt.figure(layout='constrained', figsize=(7, 6.25))
-    subfigs = fig.subfigures(2, 1, wspace=0.1)
-    
-    axes_top = subfigs[0].subplots(1, 2, sharey=True)
+sns.set_style('white')
 
-    for df, ax in zip([plot_crossfeeding_sat10, plot_crossfeeding_sat30],
-                      axes_top.flatten()):
-        
-        ax.hlines(0,xmin=df[x].min(),xmax=df[x].max(),
-                  color='gray',linestyle='--')
+fig = plt.figure(layout='constrained', figsize=(7, 6.25))
+subfigs = fig.subfigures(2, 1, wspace=0.1)
 
-        sns.lineplot(data = df, x = x, y = y, hue = 'Species',
-                     palette = cpal, marker = 'o', linewidth = 2.5,
-                     markersize = 8, ax = ax)
-        
-        ax.set(xlabel=None)
-        ax.set(ylabel=None)
-        ax.legend_.remove()
-    
-    subfigs[0].suptitle('Non-reciprocal cross-feeding drives commensalist and parasitic \n species interactions.',
-                        fontsize=14, fontweight = 'bold')
-    subfigs[0].supxlabel('Monoculture yield of species 1', fontsize=14)
-    axes_top[0].set_title('time = 10', fontsize=13)
-    axes_top[1].set_title('time = 30', fontsize=13)
-    
-    ####
-    
-    axes_bottom = subfigs[1].subplots(1, 2, sharey=True)
+axes_top = subfigs[0].subplots(1, 2, sharex=True, sharey=True)
 
-    for df, ax in zip([plot_inhibition_sat10, plot_inhibition_sat30],
-                      axes_bottom.flatten()):
-        
-        ax.hlines(0,xmin=df[x].min(),xmax=df[x].max(),
-                  color='gray',linestyle='--')
+for df, ax in zip([plot_crossfeeding10, plot_crossfeeding40],
+                  axes_top.flatten()):
+    
+    ax.hlines(0,xmin=df[x].min(),xmax=df[x].max(),
+              color='gray',linestyle='--')
 
-        sns.lineplot(data = df, x = x, y = y, hue = 'Species',
-                     palette = cpal, marker = 'o', linewidth = 2.5,
-                     markersize = 8, ax = ax)
-        
-        ax.set(xlabel=None)
-        ax.set(ylabel=None)
-        ax.legend_.remove()
-        
-    subfigs[1].suptitle('Cross-feeding on an inhibiting waste product drives \n mutualistic and commensalist species interactions.',
-                        fontsize=14, fontweight='bold')
-    subfigs[1].supxlabel('Monoculture yield of species 2', fontsize=14)
-    axes_bottom[0].set_title('time = 10', fontsize=13)
-    axes_bottom[1].set_title('time = 30', fontsize=13)
+    sns.lineplot(data = df, x = x, y = y, hue = 'Species',
+                 palette = cpal, marker = 'o', linewidth = 2.5,
+                 markersize = 8, ax = ax)
     
-    fig.supylabel(r'$log_2(\frac{\text{coculture yield}}{\text{monoculture yield}})$',multialignment='center',
-                  fontsize=18)
+    ax.set(xlabel=None)
+    ax.set(ylabel=None)
+    ax.legend_.remove()
     
-    sns.despine()
-    
-    plt.savefig("C:/Users/jamil/Documents/PhD/Data files and figures/Ecological-Dynamics-and-Community-Selection/Ecological Dynamics/Figures/speciesinteractions_saturating.png",
-                dpi=300,bbox_inches='tight')
-    
-    #######################################
+subfigs[0].suptitle('Non-reciprocal cross-feeding drives commensalist and parasitic \n species interactions.',
+                    fontsize=14, fontweight = 'bold')
+subfigs[0].supxlabel('Abundance of species 1 in monoculture', fontsize=14)
+axes_top[0].set_title('time = 10', fontsize=13)
+axes_top[1].set_title('time = 40', fontsize=13)
 
-if __name__ == '__main__':
+####
+
+axes_bottom = subfigs[1].subplots(1, 2, sharex=True, sharey=True)
+
+for df, ax in zip([plot_inhibition10, plot_inhibition40],
+                  axes_bottom.flatten()):
     
-    main()
+    ax.hlines(0,xmin=df[x].min(),xmax=df[x].max(),
+              color='gray',linestyle='--')
+
+    sns.lineplot(data = df, x = x, y = y, hue = 'Species',
+                 palette = cpal, marker = 'o', linewidth = 2.5,
+                 markersize = 8, ax = ax)
+    
+    ax.set(xlabel=None)
+    ax.set(ylabel=None)
+    ax.legend_.remove()
+        
+subfigs[1].suptitle('Cross-feeding on an inhibiting waste product drives \n mutualistic and commensalist species interactions.',
+                    fontsize=14, fontweight='bold')
+subfigs[1].supxlabel('Abundance of species 2 in monoculture', fontsize=14)
+axes_bottom[0].set_title('time = 10', fontsize=13)
+axes_bottom[1].set_title('time = 40', fontsize=13)
+fig.supylabel(r'$log_2\left(\frac{\text{abundance of species i in coculture}}{\text{abundance of species i in monoculture}}\right)$',multialignment='center',
+              fontsize=18)
+
+sns.despine()
+
+plt.savefig("C:/Users/jamil/Documents/PhD/Data files and figures/Ecological-Dynamics-and-Community-Selection/Ecological Dynamics/Figures/speciesinteractions.png",
+            dpi=300,bbox_inches='tight')
+
+####################
+
+#%%
+
+# Saturating 
+
+sns.set_style('white')
+
+fig = plt.figure(layout='constrained', figsize=(7, 6.25))
+subfigs = fig.subfigures(2, 1, wspace=0.1)
+
+axes_top = subfigs[0].subplots(1, 2, sharey=True)
+
+for df, ax in zip([plot_crossfeeding_sat10, plot_crossfeeding_sat40],
+                  axes_top.flatten()):
+    
+    ax.hlines(0,xmin=df[x].min(),xmax=df[x].max(),
+              color='gray',linestyle='--')
+
+    sns.lineplot(data = df, x = x, y = y, hue = 'Species',
+                 palette = cpal, marker = 'o', linewidth = 2.5,
+                 markersize = 8, ax = ax)
+    
+    ax.set(xlabel=None)
+    ax.set(ylabel=None)
+    ax.legend_.remove()
+
+subfigs[0].suptitle('Non-reciprocal cross-feeding drives commensalist species interactions.',
+                    fontsize=14, fontweight = 'bold')
+subfigs[0].supxlabel('Abundance of species 1 in monoculture', fontsize=14)
+axes_top[0].set_title('time = 10', fontsize=13)
+axes_top[1].set_title('time = 40', fontsize=13)
+
+####
+
+axes_bottom = subfigs[1].subplots(1, 2, sharey=True)
+
+for df, ax in zip([plot_inhibition_sat10, plot_inhibition_sat40],
+                  axes_bottom.flatten()):
+    
+    ax.hlines(0,xmin=df[x].min(),xmax=df[x].max(),
+              color='gray',linestyle='--')
+
+    sns.lineplot(data = df, x = x, y = y, hue = 'Species',
+                 palette = cpal, marker = 'o', linewidth = 2.5,
+                 markersize = 8, ax = ax)
+    
+    ax.set(xlabel=None)
+    ax.set(ylabel=None)
+    ax.legend_.remove()
+    
+subfigs[1].suptitle('Cross-feeding on an inhibiting waste product drives \n mutualistic and commensalist species interactions.',
+                    fontsize=14, fontweight='bold')
+subfigs[1].supxlabel('Abundance of species 2 in monoculture', fontsize=14)
+axes_bottom[0].set_title('time = 10', fontsize=13)
+axes_bottom[1].set_title('time = 40', fontsize=13)
+
+fig.supylabel(r'$log_2\left(\frac{\text{abundance of species i in coculture}}{\text{abundance of species i in monoculture}}\right)$',multialignment='center',
+              fontsize=18)
+
+sns.despine()
+
+plt.savefig("C:/Users/jamil/Documents/PhD/Data files and figures/Ecological-Dynamics-and-Community-Selection/Ecological Dynamics/Figures/speciesinteractions_saturating.png",
+            dpi=300,bbox_inches='tight')
+
+#######################################
+
+#%%
+
+# Growth rates
+fixed_growth = np.array([0.01,0.1])
+variable_growth = np.vstack([0.1,0])
+
+# Consumption rate
+consumption = np.array([[1,1],[1,0]])
+
+# Production rate
+metabolic_matrix = np.array([[0,0],[1,0]])
+production = np.array([np.zeros((2,2)),0.8*metabolic_matrix])
+
+# Influx and outflux
+influx = np.ones(2)
+outflux = 0.1*np.ones(2)
+
+######## Simulate community dynamics ##################
+
+# Non-resource-saturating effects
+
+t_end = 115
+
+yields_l = simulate_dynamics(two_species_model,
+                           fixed_growth,variable_growth,consumption,production,
+                           influx,outflux,
+                           t_end)
+fig, ax = plt.subplots(1,1)
+
+interaction_threshold = \
+    np.argmax(yields_l['Monoculture 1'].y[0,:] > yields_l['Coculture'][0].y[0,-1])
+    
+rect1 = mpl.patches.Rectangle((0, 0), t_end*interaction_threshold/len(yields_l['Monoculture 1'].y[0,:]),
+                             120, linewidth=1, edgecolor='none', facecolor='#d6fff5ff')
+rect2 = mpl.patches.Rectangle((t_end*interaction_threshold/len(yields_l['Monoculture 1'].y[0,:]), 0),
+                              t_end*(1-interaction_threshold/len(yields_l['Monoculture 1'].y[0,:])),
+                             120, linewidth=1, edgecolor='none', facecolor='#ffd6d6ff')
+ax.add_patch(rect1)
+ax.add_patch(rect2)
+    
+fig.text(0.18,0.92,'Positive interaction',fontsize=10, verticalalignment='center',
+         horizontalalignment='center', bbox=dict(facecolor='none', edgecolor='none'),
+         transform=ax.transAxes)
+fig.text(0.67,0.92,'Negative interaction',fontsize=10, verticalalignment='center',
+         horizontalalignment='center', bbox=dict(facecolor='none', edgecolor='none'),
+         transform=ax.transAxes)
+
+ax.plot(yields_l['Monoculture 1'].t,yields_l['Monoculture 1'].y[0,:].T,
+        color = '#926500ff',linewidth=3.5,
+        path_effects = [path_effects.Stroke(linewidth=5, foreground='black'),
+                        path_effects.Normal()])
+ax.plot(yields_l['Coculture'][0].t,yields_l['Coculture'][0].y[0,:].T,
+        color = '#e9a100ff',linewidth=3.5,
+        path_effects = [path_effects.Stroke(linewidth=5, foreground='black'),
+                        path_effects.Normal()])
+ 
+fig.supxlabel('time', fontsize=14)
+fig.supylabel('Abundance of species 2', fontsize=14,
+              horizontalalignment='center')
+ax.margins(x=0.025,y=0.025)
+fig.suptitle('Non-reciprocal cross-feeding can initially boost growth but reduces\ncarrying capacity',
+             fontsize=14, fontweight='bold')
+
+plt.legend(['_','_','Monoculture','Coculture'], fontsize=12, 
+           bbox_to_anchor=(1.0, 0.1), loc="center left")
+
+sns.despine()
+
+plt.savefig("C:/Users/jamil/Documents/PhD/Data files and figures/Ecological-Dynamics-and-Community-Selection/Ecological Dynamics/Figures/speciesinteractions_overtime.png",
+            dpi=300,bbox_inches='tight')
     
